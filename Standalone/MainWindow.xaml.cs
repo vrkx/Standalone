f072b4df5440a5951d3e9585636d1576e7945a43
vrkx <141280206+vrkx@users.noreply.github.com>
2025-01-06 14:30:03 -0600

    Launch

--- Standalone/MainWindow.xaml.cs
+++ Standalone/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Standalone.Properties;
+using Standalone.src.services;
 
 namespace Standalone
 {
@@ -19,11 +22,90 @@ namespace Standalone
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            Settings.Default.Username = Username.Text;
+            Settings.Default.Save();
+        }
 
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                Game game = new Game();
+                game.SelectFortnitePath();
+
+                PathFol.Content = Settings.Default.Path;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"Failed to select path: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+
+            // Check if any process has "FortniteClient-Win64-Shipping" in its process name
+            bool isFortniteRunning = processes.Any(p => p.ProcessName.Contains("FortniteClient-Win64-Shipping"));
+
+            if (isFortniteRunning)
+            {
+                // Get the process name of Fortnite (may vary slightly depending on the installation)
+                string fortniteProcessName = "FortniteClient-Win64-Shipping";
+
+                // Get all running Fortnite processes
+                Process[] fortniteProcesses = Process.GetProcessesByName(fortniteProcessName);
+
+                // Close each Fortnite process
+                foreach (Process process in fortniteProcesses)
+                {
+                    try
+                    {
+
+                        process.Kill();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle exceptions (e.g., access denied)
+
+                    }
+                }
+
+                Launch.Content = "Launch";
+
+
+            }
+            else
+            {
+
+                try
+                {
+                    Game game = new Game();
+                    game.InitializeFortnite();
+
+                    Launch.Content = "Launching..";
+                    Thread.Sleep(1000);
+
+                    Launch.Content = "Close";
+
+                    Console.WriteLine("User is Logged In!");
+                }
+                catch (Exception ex)
+                {
+                    Launch.Content = "Launch";
+
+                    System.Windows.MessageBox.Show($"Error starting Fortnite: {ex.Message}");
+                }
+            }
         }
     }
 }
---
+++ Standalone/src/service/Game.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+using System.Net;
+using Standalone.Properties;
+using Stellar.Assets.Services;
+
+namespace Standalone.src.services
+{
+    internal class Game
+    {
+
+
+        public void SelectFortnitePath()
+        {
+            using (CommonOpenFileDialog dialog = new CommonOpenFileDialog())
+            {
+                dialog.Multiselect = false;
+                dialog.Title = "Selesct the Folder where FortniteGame and Engine are located";
+                dialog.IsFolderPicker = true;
+                dialog.EnsurePathExists = true;
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    if (!Directory.Exists(dialog.FileName + "\\FortniteGame"))
+                    {
+                        System.Windows.MessageBox.Show("Error: Path is incorrect, please check if your season is correctly installed !");
+                        return;
+
+                    }
+                    else
+                    {
+
+                        Settings.Default.Path = dialog.FileName;
+                        Settings.Default.Save();
+                    }
+                }
+            }
+        }
+
+        public void InitializeFortnite()
+        {
+            string gamePath = Settings.Default.Path;
+
+   
+            Thread launchThread = new Thread(() =>
+            {
+                SuspendFortniteProcess(gamePath, "FortniteGame/Binaries/Win64/FortniteLauncher.exe", true);
+                SuspendFortniteProcess(gamePath, "FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping_EAC.exe", true);
+                StartFortnite(gamePath);
+            });
+
+            launchThread.Start();
+        }
+
+        private void SuspendFortniteProcess(string gamePath, string relativePath, bool suspend)
+        {
+            try
+            {
+                Process process = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = Path.Combine(gamePath, relativePath),
+                        UseShellExecute = true
+                    }
+                };
+                process.Start();
+
+                if (suspend)
+                {
+                    SuspendProcess.Suspend(process);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"Erreur lors de la suspension du processus : {ex.Message}");
+            }
+        }
+
+        private Process StartFortnite(string gamePath)
+        {
+            string launcherPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string dllPath = Path.Combine(launcherPath, "Starfall.dll");
+            string MemoryPath = Path.Combine(launcherPath, "memory.dll");
+            string ConsolePath = Path.Combine(launcherPath, "console.dll"); 
+            {
+                try
+                {
+                    if (!File.Exists(dllPath))
+                    {
+                        System.Windows.MessageBox.Show("DLL Not found, Please reinstall the launcher.");
+
+                    }
+                    else
+                    {
+                        Process Fortnitegame = new Process(); // Initialize Fortnite process
+                        Fortnitegame.StartInfo.FileName = Path.Combine(gamePath, "FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe"); // Locate fortnite path by using Settings.Default.Path
+                        Fortnitegame.StartInfo.Arguments = $@"-epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -skippatchcheck -nobe -fromfl=eac -fltoken=3db3ba5dcbd2e16703f3978d -caldera=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiYmU5ZGE1YzJmYmVhNDQwN2IyZjQwZWJhYWQ4NTlhZDQiLCJnZW5lcmF0ZWQiOjE2Mzg3MTcyNzgsImNhbGRlcmFHdWlkIjoiMzgxMGI4NjMtMmE2NS00NDU3LTliNTgtNGRhYjNiNDgyYTg2IiwiYWNQcm92aWRlciI6IkVhc3lBbnRpQ2hlYXQiLCJub3RlcyI6IiIsImZhbGxiYWNrIjpmYWxzZX0.VAWQB67RTxhiWOxx7DBjnzDnXyyEnX7OljJm-j2d88G_WgwQ9wrE6lwMEHZHjBd1ISJdUO1UVUqkfLdU5nofBQ -AUTH_LOGIN={"vrkx@gmail.com"} -AUTH_PASSWORD={"StandaloneLocal"} -AUTH_TYPE=epic -NOTEXTURESTREAMING -USEALLAVAILABLECORES -HTTP=WinINet -NOSPLASH";
+                        Fortnitegame.Start(); // Launch Fortnite
+
+                      
+                        ProcessExtension.InjectDll(Fortnitegame.Id, dllPath); // Inject redirector
+                        Thread.Sleep(60000);
+                        ProcessExtension.InjectDll(Fortnitegame.Id, MemoryPath); // Inject redirector
+                        ProcessExtension.InjectDll(Fortnitegame.Id, ConsolePath); // Inject redirector
+
+                        return Fortnitegame;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show($"Error starting Fortnite: {ex.Message}");
+                }
+            }
+            return null;
+        }
+    }
+}
